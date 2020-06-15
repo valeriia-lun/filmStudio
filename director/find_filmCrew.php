a2f1865e6ba220f5845131c35eea49bb70cc5566
valeriia-lun <61375793+valeriia-lun@users.noreply.github.com>
2020-06-15 16:16:29 +0300

    fixed

--- director/find_filmCrew.php
+++ director/find_filmCrew.php
@@ -66,37 +66,59 @@
 <thead class="thead-dark " style="background-color: #252527;">
 <tr>
 <td>Номер знімальної групи</td>
+<td>Назва фільму</td>
 <td>Дата початку роботи знімальної групи</td>
 <td>Дата закінчення роботи знімальної групи</td>
-<td><div class = "noprint">Додати сценариста і лінійного продюсера</div></td>
 <td><div class = "noprint">Змінити інформацію</div></td>
 
 </tr></thead>
 <?php
+
 $mysqli = new mysqli("localhost","root","root","filmstudio");
 $mysqli->query("SET NAMES 'utf8'");
+
+function res($result){
+$print = "";
+ if($result)
+ {
+     $rows = mysqli_num_rows($result); // количество полученных строк
+     for ($i = 0 ; $i < $rows ; ++$i)
+     {
+         $row = mysqli_fetch_row($result);
+             for ($j = 0 ; $j < 1 ; ++$j)   $print .= "$row[$j]"."<br/>";
+     }
+ }
+ return $print;
+}
+
  $result_film_crews = $mysqli->query("SELECT * FROM `film_crew`");
 
 //$mysqli->close();
 
 while ($stroka = mysqli_fetch_array($result_film_crews)){
   $temp = $stroka['number_of_film_crew'];
+
+  $result_movie = $mysqli->query("SELECT `name_of_movie` FROM `movie` WHERE `number_of_film_crew` = $temp");
+
     echo"<tr>";
-    echo"<form action=\"add_others_to_film_crew.php\" method=\"post\">";
 
     echo"<td>" . $stroka['number_of_film_crew'] . "</td>";
+    $value = res($result_movie);
+
+    echo"<td>" . $value . "</td>";
+
     echo"<td>" . $stroka['date_start_crew'] . "</td>";
     echo"<td>" . $stroka['date_finish_film_crew'] . "</td>";
-    
-    $res = $mysqli->query("SELECT * FROM `film_crew` WHERE `date_finish_film_crew` > CURDATE() AND `number_of_film_crew` = $temp");
-    $re = mysqli_fetch_array($res);
-    if($re){
-    echo "<td>"."<div class = \"btn noprint\">"."<button class =\" btn btn-danger\" value = \"" . $stroka['number_of_film_crew'] . "\" name=\"number_of_film_crew\">Додати</button>"."</div></td></form>";
-    }else{  echo "<td></td></form>";}
 
-        echo"<form action=\"editingFilmCrew.php\" method=\"post\">";
+    // $res = $mysqli->query("SELECT * FROM `film_crew` WHERE `date_finish_film_crew` > CURDATE() AND `number_of_film_crew` = $temp");
+    // $re = mysqli_fetch_array($res);
+    // if($re){
+    // echo "<td>"."<div class = \"btn noprint\">"."<button class =\" btn btn-danger\" value = \"" . $stroka['number_of_film_crew'] . "\" name=\"number_of_film_crew\">Додати</button>"."</div></td></form>";
+    // }else{  echo "<td></td></form>";}
 
-    echo "<input type=\"hidden\" value = \"" .$stroka['number_of_film_crew'] . "\" name=\"number_of_film_crew\" >";
+        echo"<form action=\"editingMovie.php\" method=\"post\">";
+
+    echo "<input type=\"hidden\" value = \"" .$value . "\" name=\"name_of_movie\" >";
     echo "<td>"."<div class = \"btn noprint\">"."<button class =\" btn btn-danger\" name=\"editBtn\">Змінити</button>"."</div></td></form>";
 
 
